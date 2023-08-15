7fb6cad4250128451d4c2b90a19f57dc154c3a61
SANYAL <136782724+sanygit@users.noreply.github.com>
2023-08-15 06:31:46 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,305 @@
+$('#slider-1.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false, 
+    smartSpeed: 1300,
+    navText:["<span class='fa fa-chevron-left'></span>", "<span class='fa fa-chevron-right'></span>"],
+    autoplay:true,
+    autoplayTimeout:5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#slider-2.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    margin:20, 
+    smartSpeed: 1300,
+    navText:["<span class='bi bi-arrow-left'<span>" ,"<span class='bi bi-arrow-right' <span>"],
+    autoplay:true,
+    autoplayTimeout:6000,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        450:{
+            items:2
+        },
+
+
+        560:{
+            items:2
+        },
+
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+$('#slider-3.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    margin:20, 
+    smartSpeed: 1300,
+    navText:["<span class='bi bi-arrow-left'<span>" ,"<span class='bi bi-arrow-right' <span>"],
+    autoplay:true,
+    autoplayTimeout:7000,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        450:{
+            items:2
+        },
+
+
+        560:{
+            items:2
+        },
+
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('#slider-4.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    margin:20, 
+    smartSpeed: 1300,
+    navText:["<span class='bi bi-arrow-left'<span>" ,"<span class='bi bi-arrow-right' <span>"],
+    autoplay:true,
+    autoplayTimeout:8000,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        450:{
+            items:2
+        },
+
+
+        560:{
+            items:2
+        },
+
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('#slider-5.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    margin:20, 
+    smartSpeed: 1300,
+    navText:["<span class='bi bi-arrow-left'<span>" ,"<span class='bi bi-arrow-right' <span>"],
+    autoplay:true,
+    autoplayTimeout:9000,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        450:{
+            items:2
+        },
+
+
+        560:{
+            items:2
+        },
+
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('#slider-6.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    margin:20, 
+    smartSpeed: 1300,
+    navText:["<span class='bi bi-arrow-left'<span>" ,"<span class='bi bi-arrow-right' <span>"],
+    autoplay:true,
+    autoplayTimeout:5000,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        450:{
+            items:2
+        },
+
+
+        560:{
+            items:2
+        },
+
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('#slider-7.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    margin:20, 
+    smartSpeed: 1300,
+    navText:["<span class='bi bi-arrow-left'<span>" ,"<span class='bi bi-arrow-right' <span>"],
+    autoplay:true,
+    autoplayTimeout:6000,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        450:{
+            items:2
+        },
+
+
+        560:{
+            items:2
+        },
+
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('#slider-8.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    margin:20, 
+    smartSpeed: 1300,
+    navText:["<span class='bi bi-arrow-left'<span>" ,"<span class='bi bi-arrow-right' <span>"],
+    autoplay:true,
+    autoplayTimeout:7000,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        450:{
+            items:2
+        },
+
+
+        560:{
+            items:2
+        },
+
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+$('#slider-9.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    margin:10, 
+    smartSpeed: 1300,
+    navText:["<span class='fa fa-chevron-left'></span>", "<span class='fa fa-chevron-right'></span>"],
+    autoplay:true,
+    autoplayTimeout:5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#slider-10.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    margin:30, 
+    smartSpeed: 1300,
+    navText:["<span class='bi bi-arrow-left'<span>" ,"<span class='bi bi-arrow-right' <span>"],
+    autoplay:true,
+    autoplayTimeout:7000,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        450:{
+            items:1
+        },
+
+
+        560:{
+            items:1
+        },
+
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+
+$(document).ready(function(){
+    $(this).scrollTop(0)
+})
